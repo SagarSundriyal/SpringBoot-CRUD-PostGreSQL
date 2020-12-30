68b33234eeac7671a6b3646e860f8594d2cc6b74
Sagar Sundriyal <62755890+SagarSundriyal@users.noreply.github.com>
2020-12-30 11:01:24 -0500

    Add files via upload

---
+++ SpringBoot-PostGreSQL-Hibernate-CRUD/src/main/java/com/company/springboot/SpringBootPostGreSqlHibernateCrudApplication.java
@@ -0,0 +1,13 @@
+package com.company.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPostGreSqlHibernateCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPostGreSqlHibernateCrudApplication.class, args);
+	}
+
+}
---
+++ SpringBoot-PostGreSQL-Hibernate-CRUD/src/main/java/com/company/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.company.springboot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.company.springboot.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee , Long>{
+
+}
